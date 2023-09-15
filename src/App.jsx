02df65da0e01eeb7eab7d9995f734f7297c5782a
NekoJar <mzharfanhakim7@gmail.com>
2023-09-15 22:17:11 +0700

    creating a global styles with styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { styled } from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
 const H1 = styled.h1`
   font-size: 30px;
@@ -6,47 +9,32 @@ const H1 = styled.h1`
   background-color: yellow;
 `;
 
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  cursor: pointer;
-
-  margin: 20px;
-`;
-
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
-`;
-
 const StyledApp = styled.div`
   background-color: orangered;
   padding: 20px;
+  box-shadow: var(--shadow-sm);
 `;
 
 function App() {
   return (
-    <StyledApp>
-      <H1>The Wild Oasis</H1>
-      <Button onClick={() => alert("check in")}>Check In</Button>
-      <Button onClick={() => alert("check out")}>Check Out</Button>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert("check in")}>Check In</Button>
+        <Button onClick={() => alert("check out")}>Check Out</Button>
 
-      <Input type="number" placeholder="Number of guests"></Input>
-      <Input type="number" placeholder="Number of guests"></Input>
-      <Input type="number" placeholder="Number of guests"></Input>
-      <Input type="number" placeholder="Number of guests"></Input>
-      <Input type="number" placeholder="Number of guests"></Input>
-      <Input type="number" placeholder="Number of guests"></Input>
+        <Input type="number" placeholder="Number of guests"></Input>
+        <Input type="number" placeholder="Number of guests"></Input>
+        <Input type="number" placeholder="Number of guests"></Input>
+        <Input type="number" placeholder="Number of guests"></Input>
+        <Input type="number" placeholder="Number of guests"></Input>
+        <Input type="number" placeholder="Number of guests"></Input>
 
-      <Input type="number" placeholder="Number of guests"></Input>
-      <Input type="number" placeholder="Number of guests"></Input>
-    </StyledApp>
+        <Input type="number" placeholder="Number of guests"></Input>
+        <Input type="number" placeholder="Number of guests"></Input>
+      </StyledApp>
+    </>
   );
 }
 
