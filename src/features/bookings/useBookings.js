ffc2911945e5ca9bc02d0e8360ab957deb8c722c
NekoJar <mzharfanhakim7@gmail.com>
2023-09-28 11:51:05 +0700

    sorting booking using API-side sorting

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -11,13 +11,17 @@ export function useBookings() {
       ? null
       : { field: "status", value: filterValue };
 
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings", filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isLoading, error, bookings };
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -8,8 +8,12 @@ export async function getBookings({ filter, sortBy }) {
       "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)",
     );
 
-  if (filter !== null)
-    query = query[filter.method || "eq"](filter.field, filter.value);
+  if (filter) query = query[filter.method || "eq"](filter.field, filter.value);
+
+  if (sortBy)
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === "asc",
+    });
 
   const { data, error } = await query;
 
