aaf251b4c6bfd0628f4ce1911bb17e6237151ba6
NekoJar <mzharfanhakim7@gmail.com>
2023-11-25 18:10:07 +0700

    add dashboard layout

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,13 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>STAT</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
