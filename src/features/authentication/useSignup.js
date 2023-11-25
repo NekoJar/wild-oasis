3c38b39b587db149bbcd8484639c65005faa41e7
NekoJar <mzharfanhakim7@gmail.com>
2023-11-25 11:58:49 +0700

    buil the app header

--- src/features/authentication/useSignup.js
+++ src/features/authentication/useSignup.js
@@ -1,7 +1,6 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { useNavigate } from "react-router-dom";
-import { signup as signupApi } from "../../services/apiAuth";
+import { useMutation } from "@tanstack/react-query";
 import toast from "react-hot-toast";
+import { signup as signupApi } from "../../services/apiAuth";
 
 export function useSignup() {
   const { mutate: signup, isLoading } = useMutation({
---
+++ src/ui/HeaderMenu.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+import ButtonIcon from "./ButtonIcon";
+import { useNavigate } from "react-router-dom";
+import { HiOutlineUser } from "react-icons/hi2";
+import Logout from "../features/authentication/Logout";
+
+const StyledHeaderMenu = styled.ul`
+  display: flex;
+  gap: 0.4rem;
+`;
+
+const HeaderMenu = () => {
+  const navigate = useNavigate();
+
+  return (
+    <StyledHeaderMenu>
+      <li>
+        <ButtonIcon onClick={() => navigate("/account")}>
+          <HiOutlineUser />
+        </ButtonIcon>
+      </li>
+      <li>
+        <Logout />
+      </li>
+    </StyledHeaderMenu>
+  );
+};
+
+export default HeaderMenu;
